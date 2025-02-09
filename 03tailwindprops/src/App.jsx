1452223c1e977497530799e24b18fed7093e6b6a
Arifk-comit <khanarifshamshul@gmail.com>
2025-02-09 10:44:30 +0530

    committed

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  let newobj = {
+    username: "Arif" ,
+    Age: 21
+  }
+  let newobj2 = {
+    username2: "Arif's Desk" ,
+    Age2: 21
+  }
+  let newobj3 = {
+    username3: "Saqif's Desk" ,
+    Age3: 21
+  }
+  let newarr = [1,2,3]
+  return (
+    <>
+     <h1 className='bg-green-400 text-white rounded-xl p-4'> Hello Tailwind</h1>
+     <Card username = "Arif"  someobj = {newobj} someobj2 = {newobj2.username2}/>
+     <Card someobj2={newobj3.username3} />
+
+    </>
+  )
+}
+
+export default App
